8f470dfb95907128e6d153c8c91a615b7bada02b
Gay Stephenson <gaystephenson@me.com>
2019-05-02 15:04:39 -0700

    add on.change and .each to append articles in Div

--- js/script.js
+++ js/script.js
@@ -1,25 +1,35 @@
 // connect to NYT API for news articles
-$.ajax({
-    method: 'get',
-    url: 'https://api.nytimes.com/svc/topstories/v2/science.json?api-key=IUALhLCmsVYpEVPVBz1PSFSRnqPN2qpJ'
-
-})
-  .done(function(data){
-    console.log(data);
-    $.each(data.results, function(index, value) {
-    $(".news-articles").append(value.section);
-        console.log(value);
-    })
 
 
-  }).fail(function(){
-      console.log('fail');
-  });
 
-
-  $('#news-select').on('change', function() {
+$('#news-select').on('change', function () {
     const selected = $(this).val();
     if (selected !== '') {
-      console.log('The value you picked is: ' + selected);
-    }
-  });
+        console.log('change');
+        console.log('The value you picked is: ' + selected);
+
+        $.ajax({
+            method: 'get',
+            url: 'https://api.nytimes.com/svc/topstories/v2/' +  selected + '.json?api-key=IUALhLCmsVYpEVPVBz1PSFSRnqPN2qpJ'
+
+
+        })
+            .done(function (data) {
+                console.log(data);
+                $.each(data.results, function (index, value) {
+                    $(".news-articles").append(`
+                        <li>
+                            <h3>${value.title}</h3>
+                            <p>${value.abstract}</p>
+                            <p>${value.multimedia, [0]}</>
+                        </li>
+                    `);
+                    console.log(value);
+                })
+            }).fail(function () {
+                console.log('fail');
+            });
+
+    }// if selected !== ''
+});// on change event
+//<img src=${value.url}>
